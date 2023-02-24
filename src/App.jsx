d31203f48a679a99b66713aa6a2578bf3ef74552
omolaso <adextee001@gmail.com>
2023-02-24 04:51:10 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+  // https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+  return <h1 className=" bg-orange-700">HI</h1>;
+}
+
+export default App;
